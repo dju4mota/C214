4a91402c3c526a8819e788d34ab3d73f88dc73b6
dju4mota <fernandomotajunior2@gmail.com>
2024-03-24 18:05:50 -0300

    constantes para mock

--- ServidorWeb-TesteMock/src/test/java/HorarioConst.java
+++ ServidorWeb-TesteMock/src/test/java/HorarioConst.java
@@ -1,5 +1,44 @@
 public class HorarioConst {
 
-    public static String  CHRIS = "";
+    public static String  CHRIS =
+            "{ \"nomeDoProfessor\": \"Christopher Lima\", \n " +
+                "\"horarioDeAtendimento\": 19:30 \n " +
+                "\"periodo\": Integral \n " +
+                "\"sala\": 22 \n " +
+                "\"predio\": \"4\" }";
 
+    public static String  RENZO =
+            "{ \"nomeDoProfessor\": \"Renzo Mesquita\", \n " +
+                    "\"horarioDeAtendimento\": 15:30 \n " +
+                    "\"periodo\": Integral \n " +
+                    "\"sala\": 30 \n " +
+                    "\"predio\": \"6\" }";
+
+    public static String  MARCELO =
+            "{ \"nomeDoProfessor\": \"Marcelo Cysneiros\", \n " +
+                    "\"horarioDeAtendimento\": 21:30 \n " +
+                    "\"periodo\": Noturno \n " +
+                    "\"sala\": 9 \n " +
+                    "\"predio\": \"2\" }";
+
+    public static String  DJU =
+            "{ \"nomeDoProfessor\": \"Fernando Mota\", \n " +
+                    "\"horarioDeAtendimento\": 15:30 \n " +
+                    "\"periodo\": Integral \n " +
+                    "\"sala\": 12 \n " +
+                    "\"predio\": \"3\" }";
+
+    public static String  LIDIA =
+            "{ \"nomeDoProfessor\": \"Lidia Andrade\", \n " +
+                    "\"horarioDeAtendimento\": 19:30 \n " +
+                    "\"periodo\": Noturno \n " +
+                    "\"sala\": 22 \n " +
+                    "\"predio\": \"4\" }";
+
+    public static String  FELIPE =
+            "{ \"nomeDoProfessor\": \"Felipe Henrique\", \n " +
+                    "\"horarioDeAtendimento\": 13:30 \n " +
+                    "\"periodo\": Integral \n " +
+                    "\"sala\": 3 \n " +
+                    "\"predio\": \"1\" }";
 }
