b5ab0a40f118f8bf146f633e5c9212d256241ced
dju4mota <fernandomotajunior2@gmail.com>
2024-03-24 18:21:59 -0300

    Mock da resposta do servidor para busca

--- ServidorWeb-TesteMock/src/test/java/ServidorRemotoMock.java
+++ ServidorWeb-TesteMock/src/test/java/ServidorRemotoMock.java
@@ -1,10 +1,29 @@
+import org.example.NomeNaoEncontradoException;
+import org.example.NomeVazioException;
 import org.example.ServidorRemoto;
 
 public class ServidorRemotoMock implements ServidorRemoto {
 
     @Override
-    public String buscaAtendimento(String nome) {
+    public String buscaAtendimento(String nome) throws NomeVazioException, NomeNaoEncontradoException {
+            switch (nome){
+                case "":
+                    throw new NomeVazioException();
+                case "Christopher":
+                    return HorarioConst.CHRIS;
+                case "Renzo":
+                    return HorarioConst.RENZO;
+                case "Marcelo":
+                    return HorarioConst.MARCELO;
+                case "Fernando":
+                    return HorarioConst.DJU;
+                case "Lidia":
+                    return HorarioConst.LIDIA;
+                case "Felipe":
+                    return HorarioConst.FELIPE;
+                default:
+                    throw new NomeNaoEncontradoException();
 
-        return null;
+            }
     }
 }
