48efcab25981f68ebf97fde78fc8fe019371398d
dju4mota <fernandomotajunior2@gmail.com>
2024-03-24 20:58:53 -0300

    Exceptions corrigidas

--- ServidorWeb-TesteMock/src/main/java/org/example/NomeNaoEncontradoException.java
+++ ServidorWeb-TesteMock/src/main/java/org/example/NomeNaoEncontradoException.java
@@ -1,4 +1,5 @@
 package org.example;
 
 public class NomeNaoEncontradoException extends Exception{
+
 }
--- ServidorWeb-TesteMock/src/main/java/org/example/NomeVazioException.java
+++ ServidorWeb-TesteMock/src/main/java/org/example/NomeVazioException.java
@@ -1,4 +1,5 @@
 package org.example;
 
 public class NomeVazioException extends  Exception{
+
 }
