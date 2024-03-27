858c85a4493143275fc3852fce81339f5b0afcd9
dju4mota <fernandomotajunior2@gmail.com>
2024-03-27 16:31:11 -0300

    Correcao variaveis

--- scripts/shell-email-test.sh
+++ scripts/shell-email-test.sh
@@ -1,5 +1,5 @@
 sudo apt-get install mailutils
 echo "Fim da instalacao"
 echo "Comeco dos testes" 
-echo $email_destinatario
-mail -s "Testes no pipeline iniciados" $email_destinatario <<< "Os testes unitarios pelo pipeline comecaram"
+echo "email:  $email_destinatario"
+mail -s "Testes no pipeline iniciados" "$email_destinatario" <<< "Os testes unitarios pelo pipeline comecaram"
