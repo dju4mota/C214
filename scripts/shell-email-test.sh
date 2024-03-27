6f6a51c4890f4f793df1af9c8a4a89463f3f064b
dju4mota <fernandomotajunior2@gmail.com>
2024-03-27 17:01:00 -0300

    variavel de ambeinte

--- scripts/shell-email-test.sh
+++ scripts/shell-email-test.sh
@@ -1,6 +1,5 @@
 sudo apt-get install mailutils
 echo "Fim da instalacao"
 echo "Comeco dos testes" 
-echo "$test"
-echo "email:  $email_destinatario"
-mail -s "Testes no pipeline iniciados" "$email_destinatario" <<< "Os testes unitarios pelo pipeline comecaram"
+echo "email:  $1"
+mail -s "Testes no pipeline iniciados" "$1" <<< "Os testes unitarios pelo pipeline comecaram"
