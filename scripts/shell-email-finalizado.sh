5a5e4cd1c781c4ab647f1738dd31b05befe9566a
dju4mota <fernandomotajunior2@gmail.com>
2024-03-27 16:18:12 -0300

    variaveis de ambientes adicionadas de novo

--- scripts/shell-email-finalizado.sh
+++ scripts/shell-email-finalizado.sh
@@ -1,4 +1,4 @@
 sudo apt-get install mailutils
 echo "Fim da instalacao"
 echo "Pipeline Finalizado" 
-mail -s "Pipeline Finalizado" $EMAILDESTINATARIO <<< "O Pipeline com testes unitarios e build foi finalizado com sucesso"
+mail -s "Pipeline Finalizado" ${vars.EMAILDESTINATARIO} <<< "O Pipeline com testes unitarios e build foi finalizado com sucesso"
--- scripts/shell-email-test.sh
+++ scripts/shell-email-test.sh
@@ -1,4 +1,4 @@
 sudo apt-get install mailutils
 echo "Fim da instalacao"
 echo "Comeco dos testes" 
-mail -s "Testes no pipeline iniciados" $EMAILDESTINATARIO <<< "Os testes unitarios pelo pipeline comecaram"
+mail -s "Testes no pipeline iniciados" ${vars.EMAILDESTINATARIO} <<< "Os testes unitarios pelo pipeline comecaram"
