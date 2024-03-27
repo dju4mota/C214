55966ad48bdf2782866b584b767a81a1d6775a99
dju4mota <fernandomotajunior2@gmail.com>
2024-03-27 15:59:24 -0300

    comando para email

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -3,4 +3,5 @@ cd Projeto de Teste Mock e Github Actions
 ls
 sudo apt-get install mailutils
 echo "Fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "a subject" fernando.b@ges.inatel.br <<< "message"
+echo "Mandando e-mail com mail do linux" 
+mail -s "a subject" fernando.b@ges.inatel.br <<< "message"
