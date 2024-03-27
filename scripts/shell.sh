682a0531c5d5c0aa74e56b0a7c072cd17262b183
dju4mota <fernandomotajunior2@gmail.com>
2024-03-27 16:06:17 -0300

    scripts de emails e novo job

--- scripts/shell.sh
+++
@@ -1,7 +0,0 @@
-ls
-cd Projeto de Teste Mock e Github Actions
-ls
-sudo apt-get install mailutils
-echo "Fim da instalacao"
-echo "Mandando e-mail com mail do linux" 
-mail -s "a subject" fernando.b@ges.inatel.br <<< "message"
